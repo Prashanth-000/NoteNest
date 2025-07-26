50caec7e0b2998e7558760d9f228a80aa6390b55
prashanth <151809864+Prashanth-000@users.noreply.github.com.>
2025-07-26 13:03:35 +0530

    controllers for view and adding new note has been added

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,8 +1,26 @@
-export const viewNotes=(req,res)=>{
-    res.status(200).send("All records are here check");
+import Note from "../models/note.js";
+
+export const viewNotes=async (req,res)=>{
+    try {
+        const notes=await Note.find();
+        res.status(200).json(notes);
+    } catch (error) {
+        console.error("Error getting all notes",error);
+        res.status(500).json({message:"Internal server error"});
+    }
+
 }
-export const addNotes=(req,res)=>{
-    res.status(201).json({message:"notes created successfully"});
+export const addNotes=async (req,res)=>{
+    try {
+        const {title,content}=req.body;
+        const newNote=new Note({title:title,content:content});
+        await newNote.save();
+        res.status(201).json({message:"Note created successfully!"});
+        
+    } catch (error) {
+        cosole.error("Error aadding new note");
+        res.status(500).json("Internal server error");
+    }
 }
 export const editNotes=(req,res)=>{
     res.status(200).json({message:"notes updated successfully"});
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,19 @@
 import express from "express";
-import notesRoute from "./routes/notesRoutes.js";
+import notesRoute from "./routers/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 
 const app=express()
 
 dotenv.config();
-const PORT=process.env.PORT || 5001
+const PORT=process.env.PORT || 5000
 
+app.use(express.json());
 app.use("/api/notes",notesRoute)
 
 connectDB();
 
-app.listen(PORT, (req,res)=>{
+app.listen(PORT, ()=>{
     console.log("Server running at port:",PORT);
 });
 
