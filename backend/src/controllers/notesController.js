ea9896239b4e0f3c4b2249882ed5a237e95906f1
prashanth <151809864+Prashanth-000@users.noreply.github.com.>
2025-07-26 16:07:20 +0530

    Programmed edit and delete functionalities of notes controller

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -18,13 +18,35 @@ export const addNotes=async (req,res)=>{
         res.status(201).json({message:"Note created successfully!"});
         
     } catch (error) {
-        cosole.error("Error aadding new note");
+        cosole.error("Error adding new note");
         res.status(500).json("Internal server error");
     }
 }
-export const editNotes=(req,res)=>{
-    res.status(200).json({message:"notes updated successfully"});
+export const editNotes=async (req,res)=>{
+    try {
+        const {title,content}=req.body;
+        const updatedNote=await Note.findByIdAndUpdate(req.params.id,
+        {title:title,content:content},
+        {new:true});
+        if(!updatedNote){
+            res.status(404).json({message:"Note not found"});
+        }
+        res.status(200).json(updatedNote);
+
+    } catch (error) {
+        cosole.error("Error editing note");
+        res.status(500).json({message:"Internal server error"});
+    }
 }
-export const deleteNotes=(req,res)=>{
-    res.status(200).json({message:"Notes deleted succesfully"})
+export const deleteNotes=async (req,res)=>{
+    try {
+        const deletedNote=await Note.findByIdAndDelete(req.params.id);
+        if(!deletedNote){
+            res.status(404).json({message:"Note not found"});
+        }
+        res.status(200).json(deletedNote);
+    } catch (error) {
+        cosole.error("Error Deleting note");
+        res.status(500).json({message:"Internal server error"});
+    }
 }
