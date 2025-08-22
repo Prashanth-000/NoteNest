06a70b7746a9a2cfc9b245baa0112ecee81a4150
prashanth <151809864+Prashanth-000@users.noreply.github.com.>
2025-08-22 23:53:53 +0530

    Added react-hot-toast to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import {Routes,Route} from "react-router"
 import DetailsPage from "./pages/DetailsPage";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
+import toast from "react-hot-toast";
 
 const App=()=>{
   return(
     <div>
+      <button onClick={()=>toast.success('Successfully toasted!')}>click me</button>
       <Routes>
         <Route path='/'element={<HomePage />} />
         <Route path='/create' element={<CreatePage />} />
